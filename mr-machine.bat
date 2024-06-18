3c64007c136c70522f922aa9eae38abf9edd424a
narr <narr@cinamon.io>
2024-06-18 18:15:24 +0900

    Update mr-machine.bat script to prompt for branch name, commit hash, assignee, title, and description

--- mr-machine.bat
+++ mr-machine.bat
@@ -18,6 +18,14 @@ git branch %branch_name% origin/develop
 
 git switch %branch_name%
 
+git push -u origin %branch_name%
+
 git cherry-pick -X theirs %commit_hash%
 
-git push origin %branch_name%      -o merge_request.create      -o merge_request.target=develop      -o merge_request.remove_source_branch      -o merge_request.title=%title%      -o merge_request.description=%description%      -o merge_request.assign="narr"
+for /f "delims=" %%i in ('git diff-tree --no-commit-id --name-only -r %commit_hash%') do (
+    set "file=%%i"
+    echo Unlocking file: %file%
+    git lfs unlock -f %file%
+)
+
+git push origin %branch_name% -o merge_request.create -o merge_request.target=develop -o merge_request.remove_source_branch -o merge_request.title=%title% -o merge_request.description=%description% -o merge_request.assign="narr"
