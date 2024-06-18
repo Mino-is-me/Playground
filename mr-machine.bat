ee953e2cf80c61c2a1ce188b94f0146791bf7709
narr <narr@cinamon.io>
2024-06-18 15:51:34 +0900

    Update mr-machine.bat script to prompt for branch name, commit hash, assignee, title, and description

--- mr-machine.bat
+++ mr-machine.bat
@@ -1,3 +1,6 @@
+
+@echo off
+
 cd ..
 cd CINEVStudio
 
@@ -7,14 +10,14 @@ set /p assignee=Enter assignee:
 set /p title=Enter title: 
 set /p description=Enter description: 
 
-git fetch --all 
+git fetch --all
+
+git stash 
 
 git branch %branch_name% origin/develop 
 
 git switch %branch_name%
 
-git pull origin %branch_name%
-
 git cherry-pick -X theirs %commit_hash%
 
-git push origin %branch_name%      -o merge_request.create      -o merge_request.target=develop      -o merge_request.remove_source_branch      -o merge_request.title=%title%      -o merge_request.description=%description%      -o merge_request.assign=%assignee%
+git push origin %branch_name%      -o merge_request.create      -o merge_request.target=develop      -o merge_request.remove_source_branch      -o merge_request.title=%title%      -o merge_request.description=%description%      -o merge_request.assign="narr"
